3bd5d9172b1aabc0e91ff84a40bb374df6040f79
Amy <141649208+lameybelle@users.noreply.github.com>
2024-12-02 16:00:18 -0800

    Update bank.c

--- bank.c
+++ bank.c
@@ -1,120 +1,110 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "account.h"
 
 #define MAX_ACCOUNTS 1000
 #define MAX_TRANSACTION_LEN 256
+#define MAX_TRANSACTIONS 1000
 
+// Structure to hold transaction data
 typedef struct {
-    char account_number[100];
-    char password[100];
-    double balance;
-    double reward_rate;
-    int transaction_tracter;
-} account;
+    char type;
+    char src_account[17];
+    char password[9];
+    char dest_account[17]; // Only needed for transfer type 'T'
+    double amount;          // Amount for 'T', 'D', 'W' types
+} transaction;
 
-account* read_accounts_and_process_transactions(char* filename, int* account_count) {
+account* read(char* filename, int* account_count, transaction* transactions, int* transaction_count) {
     FILE* file = fopen(filename, "r");
     if (!file) {
-        perror("Error opening file");
+        perror("Error opening file\n");
         exit(1);
     }
 
-    // Read total number of accounts
     fscanf(file, "%d", account_count);
     account* accounts = malloc(sizeof(account) * (*account_count));
 
-    // Read accounts
+    // Read account information
     for (int i = 0; i < *account_count; i++) {
-        int index;
-        fscanf(file, "%d", &index);  // Index of the account
+        fscanf(file, "%d", &i); // Skip index line
         fscanf(file, "%s", accounts[i].account_number);
         fscanf(file, "%s", accounts[i].password);
         fscanf(file, "%lf", &accounts[i].balance);
         fscanf(file, "%lf", &accounts[i].reward_rate);
         accounts[i].transaction_tracter = 0;
+        // Initialize mutex
+        pthread_mutex_init(&accounts[i].ac_lock, NULL);
     }
 
-    // Process transactions
-    char transaction[MAX_TRANSACTION_LEN];
-    while (fgets(transaction, MAX_TRANSACTION_LEN, file) != NULL) {
-        // Remove trailing newline, if any
-        transaction[strcspn(transaction, "\n")] = 0;
-
-        if (strlen(transaction) == 0) continue;  // Skip empty lines
-
-        // Process each transaction
-        char type;
-        char src_account[100], dest_account[100], password[100];
-        double amount = 0;
-
-        sscanf(transaction, "%c", &type);
-
-        if (type == 'D') {  // Deposit
-            sscanf(transaction, "D %s %s %lf", src_account, password, &amount);
-            account* acc = find_account(accounts, *account_count, src_account);
-            if (acc && strcmp(acc->password, password) == 0) {
-                acc->balance += amount;
-                acc->transaction_tracter++;
-                printf("Deposited %.2lf into account %s\n", amount, src_account);
-            } else {
-                printf("Error: Invalid account or password for deposit.\n");
-            }
-        } else if (type == 'W') {  // Withdraw
-            sscanf(transaction, "W %s %s %lf", src_account, password, &amount);
-            account* acc = find_account(accounts, *account_count, src_account);
-            if (acc && strcmp(acc->password, password) == 0) {
-                if (acc->balance >= amount) {
-                    acc->balance -= amount;
-                    acc->transaction_tracter++;
-                    printf("Withdrew %.2lf from account %s\n", amount, src_account);
-                } else {
-                    printf("Error: Insufficient funds in account %s\n", src_account);
-                }
-            } else {
-                printf("Error: Invalid account or password for withdrawal.\n");
-            }
-        } else if (type == 'T') {  // Transfer
-            sscanf(transaction, "T %s %s %s %lf", src_account, password, dest_account, &amount);
-            account* src_acc = find_account(accounts, *account_count, src_account);
-            account* dest_acc = find_account(accounts, *account_count, dest_account);
-            if (src_acc && dest_acc && strcmp(src_acc->password, password) == 0) {
-                if (src_acc->balance >= amount) {
-                    src_acc->balance -= amount;
-                    dest_acc->balance += amount;
-                    src_acc->transaction_tracter++;
-                    dest_acc->transaction_tracter++;
-                    printf("Transferred %.2lf from %s to %s\n", amount, src_account, dest_account);
-                } else {
-                    printf("Error: Insufficient funds in account %s\n", src_account);
-                }
-            } else {
-                printf("Error: Invalid account(s) or password for transfer.\n");
-            }
-        } else if (type == 'C') {  // Check balance
-            sscanf(transaction, "C %s %s", src_account, password);
-            account* acc = find_account(accounts, *account_count, src_account);
-            if (acc && strcmp(acc->password, password) == 0) {
-                printf("Account %s balance: %.2lf\n", src_account, acc->balance);
-            } else {
-                printf("Error: Invalid account or password for balance check.\n");
-            }
-        } else {
-            printf("Error: Unknown transaction type %c\n", type);
+    // Read transaction data
+    *transaction_count = 0;
+    char line[MAX_TRANSACTION_LEN];
+    while (fgets(line, sizeof(line), file)) {
+        // Ignore empty lines or invalid format
+        if (line[0] == '\n' || line[0] == '#') continue;
+        
+        transaction t;
+        sscanf(line, "%c %s %s", &t.type, t.src_account, t.password);
+        if (t.type == 'T') {
+            sscanf(line + strlen(line), "%s %lf", t.dest_account, &t.amount);
+        } else if (t.type == 'D' || t.type == 'W') {
+            sscanf(line + strlen(line), "%lf", &t.amount);
         }
+        
+        transactions[*transaction_count] = t;
+        (*transaction_count)++;
     }
 
     fclose(file);
     return accounts;
 }
 
-account* find_account(account* accounts, int account_count, char* account_number) {
-    for (int i = 0; i < account_count; i++) {
-        if (strcmp(accounts[i].account_number, account_number) == 0) {
-            return &accounts[i];
+void process_transaction(account* accounts, int account_count, transaction* transactions, int transaction_count) {
+    for (int i = 0; i < transaction_count; i++) {
+        transaction t = transactions[i];
+        account* account = find_account(accounts, account_count, t.src_account);
+
+        if (!account) {
+            printf("Account %s not found\n", t.src_account);
+            continue;
+        }
+
+        if (strcmp(account->password, t.password) != 0) {
+            printf("Invalid password\n");
+            continue;
+        }
+
+        if (t.type == 'T') {
+            account* dest_account_ptr = find_account(accounts, account_count, t.dest_account);
+            if (!dest_account_ptr) {
+                printf("Destination account %s not found\n", t.dest_account);
+                continue;
+            }
+
+            account->balance -= t.amount;
+            dest_account_ptr->balance += t.amount;
+            account->transaction_tracter++;
+            dest_account_ptr->transaction_tracter++;
+        } else if (t.type == 'D') {
+            account->balance += t.amount;
+            account->transaction_tracter++;
+        } else if (t.type == 'W') {
+            account->balance -= t.amount;
+            account->transaction_tracter++;
+        } else if (t.type == 'C') {
+            printf("%s balance: %.2lf\n", t.src_account, account->balance);
+        } else {
+            printf("Unknown transaction type %c\n", t.type);
         }
     }
-    return NULL;
+}
+
+void print_balances(account* accounts, int account_count) {
+    for (int i = 0; i < account_count; i++) {
+        printf("Account %d: %s, Balance: %.2lf\n", i, accounts[i].account_number, accounts[i].balance);
+    }
 }
 
 int main(int argc, char* argv[]) {
@@ -123,10 +113,18 @@ int main(int argc, char* argv[]) {
         return 1;
     }
 
-    int account_count;
-    account* accounts = read_accounts_and_process_transactions(argv[1], &account_count);
+    int account_count, transaction_count;
+    transaction transactions[MAX_TRANSACTIONS];
+    
+    account* accounts = read(argv[1], &account_count, transactions, &transaction_count);
 
-    // Free memory
+    // Process transactions
+    printf("Processing transactions...\n");
+    process_transaction(accounts, account_count, transactions, transaction_count);
+
+    // Optionally print balances
+    print_balances(accounts, account_count);
+
     free(accounts);
 
     return 0;
